cfccca42689d5921ae4443f8ca2f8ec8f1ad9e82
DRÅメDÈMØN <maheshwarisubhash843@gmail.com>
2024-10-14 22:53:00 +0530

    initial commit

---
+++ Lec12/script.js
@@ -0,0 +1,24 @@
+{
+  var studentId = 3456; //scope function
+  let studentName = "aryan"; //scope block
+  // console.log(studentName);
+  const fees = 7000000; //scope block
+  // console.log(fees);
+}
+
+console.log(studentId);
+console.log(studentName);
+console.log(fees);
+
+// DATATYPES
+// => Premitive
+// Number
+// StringBoolean
+// Null
+// Undefined
+// Symbol
+// BigInt
+
+// =>Non-Premitive
+// Array
+// Object
---
+++ Lec13/script.js
@@ -0,0 +1,19 @@
+let a = 12.34324;
+
+console.log(typeof a);
+
+let name = "aryan";
+console.log(typeof name);
+
+let flag = true;
+console.log(typeof flag);
+
+let salary;
+console.log(typeof salary);
+
+let id = null;
+console.log(typeof id);
+
+let bignumber = BigInt("21646129265961356");
+console.log(typeof bignumber);
+console.log(bignumber);
